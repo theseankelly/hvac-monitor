b46a505bc7ab31efd01f6b191f5c4c882947dbac
Sean Kelly <theseankelly@outlook.com>
2019-02-23 05:35:55 -0500

    Rewrote the main loop

--- hvacmon/__init__.py
+++ hvacmon/__init__.py
@@ -3,39 +3,61 @@
 import cv2
 import datetime
 import time
-import schedule
+import dateutil.parser
 
 import hvacmon.camera
 import hvacmon.imgproc
 import hvacmon.db
 
-def capture_state():
-    print("Capturing State...")
-
-    #
-    # Capture a frame
-    #
-    with hvacmon.camera.Camera(rotation=180) as cam:
-        im, timestamp = cam.get_frame()
-
-    #
-    # Parse the status from the image
-    #
-    status = hvacmon.imgproc.parse_image(im)
-
-    #
-    # Temporary -- log all 'raw' data from the camera until
-    # some confidence has been gained.
-    #
-    cv2.imwrite('/home/pi/data/hvac/%s.jpg' % timestamp, im)
-
-    #
-    # Insert the data into the database
-    #
-    hvacmon.db.append_zone_data(timestamp, status)
-
 def main():
-    schedule.every(15).seconds.do(capture_state)
-    while 1:
-        schedule.run_pending()
-        time.sleep(1)
+
+    with hvacmon.camera.Camera(rotation=180) as cam:
+
+        # Get the original snapshot of the system
+        im, prev_timestamp = cam.get_frame()
+        prev_status = hvacmon.imgproc.parse_image_hardcoded_positions(im)
+
+        print("(%s) Initial system state:" % prev_timestamp)
+        print(prev_status)
+
+        while 1:
+            time.sleep(5)
+            im, timestamp = cam.get_frame()
+
+            try:
+                status = hvacmon.imgproc.parse_image_hardcoded_positions(im)
+            except:
+                print("(%s) Error processing image!" % timestamp)
+                cv2.imwrite('/home/pi/data/hvac/failures/%s.jpg' % filename, im)
+                continue
+
+            dt = (dateutil.parser.parse(timestamp) -
+                  dateutil.parser.parse(prev_timestamp)).total_seconds()
+
+            filename  = dateutil.parser.parse(timestamp).strftime(
+                "%Y-%m-%d_%H-%M-%S")
+
+            if (~(status == prev_status).all()):
+                print("(%s) Status change detected!" % timestamp)
+                print(status)
+                cv2.imwrite('/home/pi/data/hvac/statechange/%s.jpg' % filename, im)
+
+                hvacmon.db.append_zone_data(prev_timestamp, timestamp, prev_status)
+                prev_status = status
+                prev_timestamp = timestamp
+
+            elif (dt > 60):
+                print("(%s) No status change after 1 min, logging..." % timestamp)
+                ts = dateutil.parser.parse(timestamp).strftime(
+                    "%Y-%m-%d_%H-%M-%S.jpg")
+                cv2.imwrite('/home/pi/data/hvac/timeouts/%s.jpg' % filename, im)
+
+                hvacmon.db.append_zone_data(
+                    prev_timestamp, timestamp, prev_status)
+                prev_status = status
+                prev_timestamp = timestamp
+
+            else:
+                #print("(%s) No status change..." % timestamp)
+                pass
+
