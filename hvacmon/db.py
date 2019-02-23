d2c796cefb58fcbbe7d01f68b112f114af63e31e
Sean Kelly <theseankelly@outlook.com>
2019-02-23 05:25:10 -0500

    Changed db schema to include start/end time

--- hvacmon/db.py
+++ hvacmon/db.py
@@ -1,17 +1,17 @@
 import sqlite3
 
 
-def append_zone_data(datetime, zoneinfo):
+def append_zone_data(starttime, endtime, zoneinfo):
 
     with sqlite3.connect('/home/pi/databases/hvacmon.db') as db:
         cursor = db.cursor()
         cursor.execute('''
-            INSERT INTO zone_readings(datetime, one_call, one_valve, two_call,
+            INSERT INTO zone_readings(starttime, endtime, one_call, one_valve, two_call,
                                       two_valve, three_call, three_valve,
                                       four_call, four_valve)
-            VALUES(?,?,?,?,?,?,?,?,?)''', (datetime,
-                                           zoneinfo[0,0], zoneinfo[0,1],
-                                           zoneinfo[1,0], zoneinfo[1,1],
-                                           zoneinfo[2,0], zoneinfo[2,1],
-                                           zoneinfo[3,0], zoneinfo[3,1]))
+            VALUES(?,?,?,?,?,?,?,?,?,?)''', (starttime, endtime,
+                                             zoneinfo[0,0], zoneinfo[0,1],
+                                             zoneinfo[1,0], zoneinfo[1,1],
+                                             zoneinfo[2,0], zoneinfo[2,1],
+                                             zoneinfo[3,0], zoneinfo[3,1]))
         db.commit()
