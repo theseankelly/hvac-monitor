afb1223197b6304b25b42548653d5c5d38b56b62
Sean Kelly <theseankelly@outlook.com>
2019-03-04 06:18:31 -0500

    Adding entry point to setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,14 @@ from distutils.core import setup
 
 setup(
     name='hvacmon',
-    packages=[],
+    description='Monitors HVAC status on Raspberry Pi',
     version='0.1.0',
-    description='Monitors HVAC performance on Raspberry Pi',
+    packages=['hvacmon'],
+    entry_points={
+        'console_scripts': [
+            'hvacmon = hvacmon.__main__:main'
+        ]
+    },
     author='Sean Kelly',
     license='TBD',
     author_email='theseankelly@outlook.com',
